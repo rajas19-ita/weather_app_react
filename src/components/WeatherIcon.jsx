d15dff92fdbfae9f28405f8e8cc25af6dede7527
rajas19-ita <rajaskadge@gmail.com>
2024-01-16 14:30:55 +0530

    merge remote changes

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -3,12 +3,11 @@ import React, { useState } from "react";
 function WeatherIcon({ icon }) {
     const [loading, setLoading] = useState(true);
 
-    if (loading) {
-        return (
-            <div className="w-[3rem] xxs:w-[2.5rem] h-[3rem] xxs:h-[2.5rem] rounded-full bg-blue-600 animate-pulse"></div>
-        );
-    } else {
-        return (
+    return (
+        <>
+            {loading ? (
+                <div className="w-[3rem] xxs:w-[2.5rem] h-[3rem] xxs:h-[2.5rem] rounded-full bg-blue-500 animate-pulse"></div>
+            ) : null}
             <img
                 src={icon}
                 className={` max-w-[3rem] xxs:max-w-[2.5rem] ${
@@ -16,8 +15,8 @@ function WeatherIcon({ icon }) {
                 } `}
                 onLoad={() => setLoading(false)}
             />
-        );
-    }
+        </>
+    );
 }
 
 export default WeatherIcon;
